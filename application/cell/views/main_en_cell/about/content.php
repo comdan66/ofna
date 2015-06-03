d7e70117e50470ea673b13ddc3408eb3c7dc6b2f
OA Wu <comdan66@gmail.com>
2015-06-03 22:27:50 +0800

    Fix layout

--- application/cell/views/main_en_cell/about/content.php
+++ application/cell/views/main_en_cell/about/content.php
@@ -1,43 +1,29 @@
 <div id='about'>
   <div>
-    <div class='t'>
-      <div class='l'></div>
-      <div class='t'>ABOUT</div>
-      <div class='l'></div>
+    <div class='t'>ABOUT</div>
+    <div class='c'>
+      <div class='l'>起源</div>
+      <div class='r'>
+        OFNA devotes to skin care with natural ingredients harmoniously.<br/><br/>
+        Whole series hold an adherent that users are able to experience natural skin improvement in short time by simple and easy daily skin care without harmful ingredients.<br/><br/>
+        Pure herbal extractions stimulate metabolism of skin cells naturally, and strength renew ability of skin cells.<br/><br/>
+        Unite with newest bio-technology to treat particular skin and make it purely vitality and engaging.<br/><br/>
+        Several patent ingredients provided for noblest skin care. 
+      </div>
     </div>
-    <div class='bs'>
-      <div class='b'>
-        <div class='t'>起源</div>
-        <div class='c'>
-          碩荷有限公司為OFNA品牌代理公司，OFNA品牌致力於追求自然環境與保養的和諧。
-          <br />
-          <br />
-          全系列產品秉持著純淨自然無不良添加的堅持，僅需依照日常簡單的保養步驟，便可讓肌膚在最短的時間內體驗最大的改善，讓愛美女性輕鬆擁有自然無暇的肌膚。
-          <br />
-          <br />
-          天然植物萃取，讓細胞能進行自然的新陳代謝，加強細胞的再生活力，輕鬆打造自然美麗。使肌膚充滿生命力，散發自然光彩，多項專利成分，給予肌膚最高貴精緻的呵護。
-        </div>
+    <div class='c'>
+      <div class='l'>核心價值</div>
+      <div class='r'>
+        Approach to the door, access to fashion and beauty and enjoy a turn back time journey.<br/><br/>
+        <div class='i'><img src='/resource/image/site/AltE00.jpg' /><p>Perfect, Safe, and focus are our faiths of product development.</p></div>
+        <div class='i'><img src='/resource/image/site/AltE01.jpg' /><p>Professional, kind, and listen are our goals of service.</p></div>
       </div>
-      <div class='b'>
-        <div class='t'>核心價值</div>
-        <div class='c'>
-          推開通往時尚與美麗的門，享受一趟時光倒流的旅程完美、安全、專一，是我們研發產品的信念。
-          <br />
-          <br />
-          專業、親切、請聽，是我們的服務宗旨。
-        </div>
-      </div>
-      <div class='b'>
-        <div class='t'>核心價值</div>
-        <div class='c'>
-          OFNA秉持不斷研發創新的精神，不吝於投入成本，只希望能提供顧客最好的選擇，並嚴密空管品質，以確保每位顧客的權益。
-          <br />
-          <br />
-          用心以及對產品的堅持，兢兢業業的在台灣經營化妝品市場。
-          <br />
-          <br />
-          放眼未來，OFNA會更用心更積極，為所有亞洲女性創造出健康柔滑的肌膚，邁入新世紀的保養紀元。
-        </div>
+    </div>
+    <div class='c'>
+      <div class='l'>遠景</div>
+      <div class='r'>
+        OFNA insists continuously develop, generously cost on investment, and provide best choice to customers. Also we tightly control the quality and ensure the rights for each customer. OFNA cautiously but forcefully works hard and insists to the products to run the business in Taiwan.<br/><br/>
+        Looking forward to the future, OFNA will ceaselessly create healthy, smooth skin for Asian females positively. OFNA will set foot on New Age movement Of skin care with you.
       </div>
     </div>
   </div>
