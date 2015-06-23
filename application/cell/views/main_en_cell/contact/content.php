fca0c5e5bce264597b590ae717c42e0c946f2e48
OA Wu <comdan66@gmail.com>
2015-06-23 19:27:51 +0800

    Fix bug.

--- application/cell/views/main_en_cell/contact/content.php
+++ application/cell/views/main_en_cell/contact/content.php
@@ -9,14 +9,14 @@
         <div><b>T</b> +886-2-8693-1920</div>
         <div><b>F</b> +886-2-8693-1920</div>
         <div><b>E-mail</b> ofna@ofna-bio.com</div>
-        <div><b>客服時間</b> 週一 - 週五 AM09:00-PM06:00</div>
+        <div><b>Service time</b> Monday - Friday AM09: 00-PM06: 00</div>
       </div>
       <div class='r'>
-        <form class='f' data-info='您的意見我們已經收到，將盡快與您聯繫。'>
-          <input type='text' value='' name='name' placeholder='姓名' maxlength='255' pattern=".{1,255}" required title="請輸入姓名!" />
-          <input type='text' value='' name='email' placeholder='電子郵件' maxlength='255' pattern=".{1,255}" required title="請輸入電子郵件!" />
-          <textarea name='message' placeholder='留言' pattern=".{1,}" required title="請輸入留言!" ></textarea>
-          <button type='submit'>送出</button>
+        <form class='f' data-info='Your comments we have received, will contact you shortly.'>
+          <input type='text' value='' name='name' placeholder='Name' maxlength='255' pattern=".{1,255}" required title="Please enter your name!" />
+          <input type='text' value='' name='email' placeholder='Email address' maxlength='255' pattern=".{1,255}" required title="Please enter your email address!" />
+          <textarea name='message' placeholder='Message' pattern=".{1,}" required title="Please enter your message!" ></textarea>
+          <button type='submit'>Send</button>
         </form>
       </div>
     </div>
